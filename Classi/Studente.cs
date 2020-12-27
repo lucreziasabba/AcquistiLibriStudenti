06fbaca6e56a3c82b0b47cbc3885138355585000
lucrezia sabba <lucrezia.sabba@icloud.com>
2020-12-27 13:14:35 +0100

    cs

---
+++ Classi/Studente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi
+{
+    public class Studente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int nMatricola { get; set; } = 0;
+
+
+        public Studente(string n, string c, int m)
+        {
+            Nome = n;
+            Cognome = c;
+            nMatricola = m;
+        }
+
+        public string GetProprieta()
+        {
+            return $"MATR : {nMatricola} {Nome} {Cognome}";
+        }
+    }
+}
--- WpfLibriStudenti/MainWindow.xaml.cs
+++ WpfLibriStudenti/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Classi;
 
 namespace WpfLibriStudenti
 {
@@ -23,6 +24,83 @@ namespace WpfLibriStudenti
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public List<Studente> studenti = new List<Studente>();
+        public List<Libro> libri = new List<Libro>();
+
+        int matricola = 1;
+        int i = 1;
+        
+
+        private void btnCaricaStudente_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (txtNome.Text == "" || txtCognome.Text == "")
+                {
+                    MessageBox.Show("Inserire i valori", "", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    txtNome.Text = "";
+                    txtCognome.Text = "";
+                    txtNome.Focus();
+                }
+                else
+                {
+                    string nome = txtNome.Text;
+                    string cognome = txtCognome.Text;
+                    Studente s = new Studente(nome, cognome, matricola);
+                    studenti.Add(s);
+                    cmbStudenti.Items.Add(s.GetProprieta());
+                    matricola++;
+                    txtNome.Text = " ";
+                    txtCognome.Text = " ";
+                    txtNome.Focus();
+                    cmbStudenti.SelectedIndex = -1;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                txtNome.Text = " ";
+                txtCognome.Text = " ";
+                txtNome.Focus();
+            }
+        }
+
+        private void btnCaricaLibro_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (cmbMaterie.Text == "" || txtPrezzo.Text == "")
+                {
+                    MessageBox.Show("Inserire tutti i valori", "", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    cmbMaterie.Text = "";
+                    txtPrezzo.Text = "";
+                    cmbMaterie.Focus();
+                }
+                else
+                {
+                    string materia = cmbMaterie.Text;
+                    double prezzo = double.Parse(txtPrezzo.Text);
+                    string isbn = "lib2020-2021" + i.ToString();
+                    i++;
+                    Libro l = new Libro(materia, prezzo, isbn);
+
+                    libri.Add(l);
+                    cmbLibro.Items.Add(l.GetProprieta());
+                    cmbMaterie.Text = "";
+                    txtPrezzo.Text = "";
+                    cmbMaterie.Focus();
+                    cmbLibro.SelectedIndex = -1;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                cmbMaterie.Text = "";
+                txtPrezzo.Text = "";
+                cmbMaterie.Focus();
+            }
         }
     }
 }
